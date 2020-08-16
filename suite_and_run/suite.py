94e556fed32b2f1791d1f7b7a327823edab87624
tianmeng-wxk <3394788013@qq.com>
2020-08-17 01:13:22 +0800

    生成htmltestrunner报告

--- suite_and_run/suite.py
+++ suite_and_run/suite.py
@@ -26,7 +26,7 @@ def suite():
         pass
 
     with open(report_file, 'wb')as file:
-        runner = HTMLTestRunner(stream=file,verbosity=2, title="接口测试", description="接口测试")
+        runner = HTMLTestRunner(stream=file, title="接口测试", description="接口测试")
         runner.run(suite)
 
     send_email(report_file)
