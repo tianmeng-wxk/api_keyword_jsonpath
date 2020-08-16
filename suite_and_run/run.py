067e4845bf89da1403186286e6626c2753c523a3
tianmeng-wxk <3394788013@qq.com>
2020-08-17 00:45:01 +0800

    生成allure报告

--- suite_and_run/run.py
+++ suite_and_run/run.py
@@ -20,6 +20,6 @@ if __name__ == '__main__':
     #生成allure报告
     import os
     pytest.main(['-s','-q','../test_case/test_cases.py', '--alluredir=../report/allure_xml'])#生成alure缓存文件
-    send_mail('../api_keywork_jsonpath/report/allure_html/index.html')
+    send_mail('../report/allure_html/index.html')
     # os.system('allure generate --clean ../report/allure_xml/ -o ../report/allure_html/')
     # os.system('allure serve ../report/allure_xml')
